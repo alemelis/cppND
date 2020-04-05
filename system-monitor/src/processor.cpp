6b161b6f4804ceb54c3e864b30b1d2ab2301022d
alessandro <alessandro_melis@rocketmail.com>
2020-04-05 14:33:23 +0100

    CPU

--- system-monitor/src/processor.cpp
+++ system-monitor/src/processor.cpp
@@ -1,4 +1,13 @@
 #include "processor.h"
+#include "linux_parser.h"
+#include <iostream>
 
-// TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+// Return the aggregate CPU utilization
+float Processor::Utilization() { 
+	long active_jiffies = LinuxParser::ActiveJiffies();
+	long tot_jiffies = LinuxParser::Jiffies();
+	float cpu = ((float) active_jiffies - prev_active_jiffies_) / ((float) tot_jiffies - prev_tot_jiffies_);
+	prev_active_jiffies_ = active_jiffies;
+	prev_tot_jiffies_ = tot_jiffies;
+	return cpu;
+}
